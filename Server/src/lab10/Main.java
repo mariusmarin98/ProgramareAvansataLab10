fe908a6f651773393c87bfabcdd2335dc5b47305
mariusmarin98 <62063274+mariusmarin98@users.noreply.github.com>
2020-05-01 01:02:43 +0300

    Add files via upload

---
+++ Server/src/lab10/Main.java
@@ -0,0 +1,11 @@
+package lab10;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        GameServer gameServer = new GameServer("127.0.0.1",8100);
+
+    }
+}
